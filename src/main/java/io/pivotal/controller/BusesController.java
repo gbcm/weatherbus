ff39fd7dca76d08e0770b637a3e47101a4fc8523
Andres and Steve Gravrock <pair+amedina+sgravrock@pivotal.io>
2016-01-15 17:05:46 -0800

    Send correct content-type headers

--- src/main/java/io/pivotal/controller/BusesController.java
+++ src/main/java/io/pivotal/controller/BusesController.java
@@ -8,11 +8,14 @@ import io.pivotal.view.DeparturePresenter;
 import io.pivotal.view.JsonListPresenter;
 import io.pivotal.view.JsonPresenter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,7 @@ public class BusesController {
     @Autowired
     private BusService busService;
 
-    @RequestMapping("departures")
+    @RequestMapping(path = "departures", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public @ResponseBody String getDepartures(@RequestParam String stopId) throws Exception {
         List<Departure> departures = busService.getDeparturesForStop(stopId);
         List<JsonPresenter> presenters = new ArrayList<>();
@@ -34,7 +37,7 @@ public class BusesController {
         return new JsonListPresenter(presenters).toJson();
     }
 
-    @RequestMapping("coordinates")
+    @RequestMapping(path = "coordinates", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public @ResponseBody String getCoordinates(@RequestParam String stopId) throws Exception {
         Coordinate coordinate = busService.getCoordinatesForStop(stopId);
         JsonPresenter presenter = new CoordinatePresenter(stopId, coordinate);
