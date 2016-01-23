5edd65b8b3d576d994a0c68b5c1f94417933e62b
Alpha Chen, Andres Medina and Augustus Lidaka <pair+achen+amedina+alidaka@pivotal.io>
2016-01-22 16:04:00 -0800

    Changing default weather service environment to run against dev environment (instead of local dependencies)

--- src/main/java/io/pivotal/Constants.java
+++ src/main/java/io/pivotal/Constants.java
@@ -4,7 +4,7 @@ package io.pivotal;
  * Created by pivotal on 1/6/16.
  */
 public class Constants {
-    public static final String WEATHER_SERVICE_ENDPOINT = "http://localhost:9090/api";
+    public static final String WEATHER_SERVICE_ENDPOINT = "http://weatherbus-weather-dev.cfapps.io/api";
     public static final String ONEBUSAWAY_ENDPOINT = "http://api.pugetsound.onebusaway.org";
     public static final String ONEBUSAWAY_KEY = "TEST";
 }
