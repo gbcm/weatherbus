b1973ae2e8d0ffce48ce6e25adc73df4ad36d86e
Greg C-M and Steve Gravrock <pair+gchattinmcnichols+sgravrock@pivotal.io>
2016-02-25 10:12:04 -0800

    Celsius hack

--- src/main/java/io/pivotal/model/DepartureWithTemperature.java
+++ src/main/java/io/pivotal/model/DepartureWithTemperature.java
@@ -3,20 +3,30 @@ package io.pivotal.model;
 import io.pivotal.service.response.DepartureResponse;
 import lombok.Data;
 
+import java.text.DecimalFormat;
+
 @Data
 public class DepartureWithTemperature extends DepartureResponse {
     private Double temp;
     private String climacon_url;
     private String climacon;
 
-    public DepartureWithTemperature(String routeShortName, String headsign, long predictedTime, long scheduledTime, Double temperature, String climacon_url, String climacon) {
+    public DepartureWithTemperature(String routeShortName, String headsign, long predictedTime, long scheduledTime, Double tempF, String climacon_url, String climacon) {
         super(routeShortName, headsign, predictedTime, scheduledTime);
-        this.temp = temperature;
+        this.temp = fToC(tempF);
         this.climacon_url = climacon_url;
         this.climacon = climacon;
     }
 
     public DepartureWithTemperature(DepartureResponse departureResponse, Double temperature, String climacon_url, String climacon) {
         this(departureResponse.getRouteShortName(), departureResponse.getHeadsign(), departureResponse.getPredictedTime(), departureResponse.getScheduledTime(), temperature, climacon_url, climacon);
+    }
+
+    private Double fToC(Double f) {
+        DecimalFormat df = new DecimalFormat("#.#");
+        if (f == null) {
+            return null;
+        }
+        return Double.parseDouble(df.format((f - 32) * 5 / 9));
     }
 }
